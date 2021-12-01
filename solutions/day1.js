6af2b3e2b649cdb295bfac471bdcb8b6c3abf9a7
matecsornyei <mate@csornyei.com>
2021-12-01 10:01:35 +0100

    day1 - Solution for the second task

--- solutions/day1.js
+++ solutions/day1.js
@@ -9,7 +9,14 @@ function firstSolution(input) {
 }
 
 function secondSolution(input) {
-    console.log("secondSolution")
+    let increased = 0;
+    const sum = (prev, current) => prev + current;
+    for (let index = 3; index < input.length; index++) {
+        const currentWindow = [input[index], input[index - 1], input[index - 2]].reduce(sum);
+        const previousWindow = [input[index - 1], input[index - 2], input[index - 3]].reduce(sum);
+        if (currentWindow > previousWindow) increased += 1;
+    }
+    console.log(increased);
 }
 
 module.exports = {
